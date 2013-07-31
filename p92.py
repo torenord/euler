db14421909c1db9d79673daebc9ea6f4a1e7ba66
Tore Norderud <tore.norderud@gmail.com>
2013-07-31 15:30:30 +0200

    .

--- p92.py
+++ p92.py
@@ -6,14 +6,11 @@ from functools import lru_cache
 
 @lru_cache(maxsize=None)
 def f(n):
-    return sum(map(lambda x: int(x)*int(x), list(str(n))))
+    return sum([int(c)*int(c) for c in str(n)])
 
 @lru_cache(maxsize=None)
 def g(n):
-    if n == 1 or n == 89:
-        return n
-    else:
-        return g(f(n))
+    return n if n == 1 or n == 89 else g(f(n))
 
 def h(n):
     return sum([1 for i in range(1, n) if g(i) == 89])
