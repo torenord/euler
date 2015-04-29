587d8c5450ba87a9e48f1f0c3e1fb25c7f01d871
Tore Norderud <tore.norderud@gmail.com>
2015-04-29 13:57:47 +0200

    .

--- problem1.py
+++ problem1.py
@@ -8,7 +8,7 @@
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
 def problem1():
-    return sum([i for i in range(1000) if i % 3 == 0 or i % 5 == 0])
+    return sum(i for i in range(1000) if i % 3 == 0 or i % 5 == 0)
 
 if __name__ == '__main__':
     print(problem1())
